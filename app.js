1141e234f10ddf93a4834f56452448b31a914bfe
Saikrishna Panda <itssai91@gmail.com>
2021-06-06 17:05:39 +0530

    Sending Response

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const http = require('http'); // Importing Http Module
 
-const server = http.createServer((req, res) => { // Create Server takes a Function, That Function
-    console.log(req);                                      // Takes Two Parameters
+const server = http.createServer((req, res) => { // Create Server takes a Function, That Function Takes Two Parameters
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Welcome Sai</title></head>');
+    res.write('<body><h1>Hello Node JS</h1></body>');
+    res.write('/html');
+    res.end();
 });
 
 server.listen(3000);
