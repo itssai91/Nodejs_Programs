9ee5af439783534f15599fe4d1fca82b88684085
Saikrishna Panda <itssai91@gmail.com>
2021-06-06 17:17:45 +0530

    Routing Response

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const http = require('http'); // Importing Http Module
 
 const server = http.createServer((req, res) => { // Create Server takes a Function, That Function Takes Two Parameters
+
+    const url = req.url;
+    if (url === '/') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>Welcome Sai</title></head>');
+        res.write('<body><form action="/message" method="post"><input type="text" name="message" /><input type="submit" /></form></body>');
+        res.write('/html');
+        return res.end();
+    }
+
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>Welcome Sai</title></head>');
